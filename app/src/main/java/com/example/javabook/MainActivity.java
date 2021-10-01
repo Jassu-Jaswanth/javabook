0244ab362c55d7326e1267fade70bc0d38cb1b3e
Jassu-Jaswanth <alliswelljaswanth@gmail.com>
2021-10-01 23:55:21 +0530

    Calculator ready

--- app/src/main/java/com/example/javabook/MainActivity.java
+++ app/src/main/java/com/example/javabook/MainActivity.java
@@ -2,22 +2,34 @@ package com.example.javabook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1;
+    private EditText editText1, editText2;
+    private Button buttonAdd;
+    private TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button)findViewById(R.id.button1);
     }
-    public void onClick(View v){
-        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-        startActivity(intent);
+    public void onClick(View view){
+        editText1 = (EditText)findViewById(R.id.editText1);
+        editText2 = (EditText)findViewById(R.id.editText2);
+        buttonAdd = (Button)findViewById(R.id.buttonAdd);
+        result = (TextView)findViewById(R.id.result);
+
+        String string1 = editText1.getText().toString();
+        String string2 = editText2.getText().toString();
+        result.setText(String.valueOf(Integer.parseInt(string1)+Integer.parseInt(string2)) );
+        Toast.makeText(getApplicationContext(),result.getText(), Toast.LENGTH_LONG).show();
+
     }
 }
